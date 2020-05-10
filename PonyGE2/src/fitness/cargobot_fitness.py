8d718c4b4c06201d07d3d4a03c239f85001d6181
hkolanska <helenakolanska@gmail.com>
2020-05-10 15:30:50 +0200

    Add neighborhood to fitness

--- PonyGE2/src/fitness/cargobot_fitness.py
+++ PonyGE2/src/fitness/cargobot_fitness.py
@@ -24,22 +24,47 @@ class cargobot_fitness(base_ff):
 
     def check_difference(self, final, actual):
         score = 0
-        for state_a, state_b in zip(final.boardState, actual.boardState):
-            if state_a != state_b:
-                score += 1
+        for row_a, row_b in zip(final.boardState, actual.boardState):
+            for state_a, state_b in zip(row_a, row_b):
+                if state_a != state_b:
+                    score += 1
         return score
+
+    def check_difference_with_neighborhood(self, final, actual):
+        score = 0
+        for index_x, row_a in enumerate(actual.boardState):
+            for index_y, state_a in enumerate(row_a):
+                score += self.neighborhood(index_x, index_y, state_a,
+                                          final.boardState)
+        return score
+
+    def neighborhood(self, x, y, state, final):
+        if state == final[x][y]:
+            return 0
+        neighbors = [(x2, y2) for x2 in range(max(0, x-1), min(len(final), x+2))
+                    for y2 in range(max(0, y-1), min(len(final[0]), y+2))
+                    if (x2, y2) != (x, y)]
+        for a, b in neighbors:
+            if state == final[a][b]:
+                return 10
+        return 100
 
     def evaluate(self, ind, **kwargs):
         guess = ind.phenotype
         compiler = Compiler()
         print(guess)
+        if guess.count("down_arrow")<2:
+            return 10000
+        if len(guess.split()) < 14:
+            print(1000)
+            return 1000
         program = compiler.compile(guess)
         initial_board = Board(self.path, "init")
         final_board = Board(self.path, "end")
         lift = Lift(self.path)
         game = GameOperations(initial_board, final_board, program, lift)
         game.runProgram()
-        fitness = 0 if game.checkVictory() else self.check_difference(
+        fitness = 0 if game.checkVictory() else self.check_difference_with_neighborhood(
             game.finalBoard, game.board)
         print(fitness)
         return fitness
